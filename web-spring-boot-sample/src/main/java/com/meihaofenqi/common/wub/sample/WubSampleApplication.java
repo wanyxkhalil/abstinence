3622173f8a3df3e9b691aed4a7e55c7f728a23cb
wanyuxuan <wanyuxuan@meihaofenqi.com>
2019-01-03 15:12:16 +0800

    init

---
+++ web-spring-boot-sample/src/main/java/com/meihaofenqi/common/wub/sample/WubSampleApplication.java
@@ -0,0 +1,12 @@
+package com.meihaofenqi.common.wub.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WubSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WubSampleApplication.class, args);
+    }
+}
---
+++ web-spring-boot-sample/src/main/java/com/meihaofenqi/common/wub/sample/web/SampleController.java
@@ -0,0 +1,17 @@
+package com.meihaofenqi.common.wub.sample.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping("/test")
+    public Object test() {
+        return LocalDateTime.now();
+    }
+}
